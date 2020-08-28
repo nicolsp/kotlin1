1c9e2e8a5bd81c9003cc15ae4c1fd75ef1cda5d7
copito.123 <nicolspnicolsp@gmail.com>
2020-08-27 23:06:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kotlin1/main.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin1
+
+fun main(){
+println("Hola mundo mundial")
+
+
+
+    var palabra01 = "Hola que tal"
+    var number = 10
+    val sinespacios = palabra01
+    val concatenado = "El larga de la palabra 01 es: ${palabra01.length}"
+}
